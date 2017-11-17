fde7fac42fa74993a3aa02cacbfc48f96eb426e3
Vanessa Rodriguez Cristobal <vanessa@Vanessas-MacBook-Pro.local>
2017-11-17 16:25:07 +0000

    Updating API handling

--- schema.js
+++ schema.js
@@ -22,14 +22,9 @@ const RecipeType = new GraphQLObjectType({
     fields: () => ({
         label: {
             type: GraphQLString,
-            resolve: () => 
-                recipe.responseData.label
+            resolve: (data) => 
+                data.recipe.label
         },
-
-       /* source: {
-            type: GraphQLInt,
-            resolve: () => { return recipe.source; }
-        }*/
     })
 })
 
@@ -40,22 +35,21 @@ module.exports = new GraphQLSchema({
 
         fields: () => ({
             recipe: {
-                type: RecipeType,
+                type: GraphQLList(RecipeType),
                 args: {
                     id: { type: GraphQLInt }
                 },
                 resolve: (root, args) => fetch(
                     `https://api.edamam.com/search?q=`+q+`&app_ID=`+ID)
                     .then((response) => {
-                        console.log('fetching-promise');
-                       // response.json(data);
-                        //console.log(response.json());
                         return response.json();
                     }) 
                     .then((responseData) => {
-                        console.log('response.json-promise resolution');
-                        console.log(responseData.hits[0], ' data here maybe?')
-                        return responseData.hits[0];
+                        //console.log(responseData.hits[0], ' data here maybe?')
+                        for (var i = 0; i < responseData.hits.length; i++) {
+                            console.log( responseData, responseData.hits[i])
+                            return responseData.hits[i];
+                        }
                     })
             }
         })
